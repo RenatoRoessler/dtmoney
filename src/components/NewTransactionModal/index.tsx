bc0658d7ed2ec7f02564d3eebe8fbd6c7870f50e
Renato Roessler <renato@dotse.com.br>
2021-09-12 19:08:32 -0300

    first commit

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,37 @@
+import Modal from "react-modal";
+import { Container } from "./styles";
+import closeImg from "../../assets/close.svg";
+
+interface NewTransationModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransationModal({
+  isOpen,
+  onRequestClose,
+}: NewTransationModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
